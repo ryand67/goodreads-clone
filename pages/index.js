ef9a0014dde9eee3db1a6e90d5215d9f957ad40b
ryand67 <rwdevenney@gmail.com>
2021-01-06 14:34:00 -0500

    states

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,21 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import axios from 'axios';
 import BookEntry from '../components/BookEntry';
+import { useState } from 'react';
 
 export default function Home() {
-  const BOOK_URL = `https://www.googleapis.com/books/v1/volumes?q=flowers&orderBy=newest&key=AIzaSyB_-s2BfbodxkeNLMLomAGJJbaRWhwSd-k`;
+  const [searchAuthor, setSearchAuthor] = useState('');
+  const [searchTitle, setSearchTitle] = useState('');
+
+  const BOOK_URL = `https://www.googleapis.com/books/v1/volumes?q=${searchTitle}+inauthor:${searchAuthor}&key=AIzaSyB_-s2BfbodxkeNLMLomAGJJbaRWhwSd-k`;
+
+  const handleTitleChange = (e) => {
+    setSearchTitle(e.target.value);
+  }
+
+  const handleAuthorChange = (e) => {
+    setSearchAuthor(e.target.value);
+  }
 
   return (
     <div className={styles.container}>
@@ -15,12 +27,12 @@ export default function Home() {
       <form className={styles.searchForm} action="">
         <div className={styles.query}>
           <h2 className={styles.label}>Title</h2>
-          <input type="text" name="Title" id="titleInput"/>
+          <input onChange={(e) => handleTitleChange(e)} type="text" name="Title" id="titleInput"/>
         </div>
 
         <div className={styles.query}>
           <h2 className={styles.label}>Author</h2>
-          <input type="text" name="Author" id="authorInput"/>
+          <input onChange={(e) => handleAuthorChange(e)} type="text" name="Author" id="authorInput"/>
         </div>
         <div className={styles.query}>
           <button className={styles.button} type="submit">Search</button>
